ce68d21ee4243e9af22933a5e00e7d555d75dcf6
galileoguzman <galileoguzman@gmail.com>
2020-06-27 09:54:43 -0500

    Default properties for tweet

--- tweet.py
+++ tweet.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+
 class Tweet():
     def __init__(self, text, author):
-        # Instance variables
+        # Instance variables / properties
         self.text = text
         self.author = author
+        # Internal properties
+        self.created_at = datetime.now()
+        self.likes = 0
