af4f47673aecb0039620c78df1bca9c9e3140878
galileoguzman <galileoguzman@gmail.com>
2020-06-27 10:51:14 -0500

    Twitter class with timeline method printing all tweets at the moment

--- twitter.py
+++ twitter.py
@@ -1,2 +1,33 @@
+from tweet import Tweet
+
 class Twitter():
-    pass
+    def __init__(self):
+        self.tweets = [] # List of tweets
+
+    def timeline(self):
+        for t in self.tweets:
+            print(t.published_at)
+
+
+twitter = Twitter()
+print('---Twitter timeline---')
+print(f'{len(twitter.tweets)} at the moment.')
+
+USERNAME = '@galileoguzman'
+
+t1 = Tweet('Mi primer tweet!!!  ^_^', USERNAME)
+t2 = Tweet('Aprendo @python', USERNAME)
+t3 = Tweet('Hola ya vieron como tembló? -_-', USERNAME)
+
+twitter.tweets.append(t1)
+twitter.tweets.append(t2)
+twitter.tweets.append(t3)
+
+print(f'{len(twitter.tweets)} at the moment.')
+
+texto = input('Qué estas pensando?')
+nuevo_tweet = Tweet(texto, USERNAME)
+
+twitter.tweets.append(nuevo_tweet)
+print(f'{len(twitter.tweets)} at the moment.')
+twitter.timeline()
